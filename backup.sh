1664224da6181ff03e993a3071eecb55b526c4f4
Breadinator <Breadinator@users.noreply.github.com>
2017-07-02 18:58:05 +0100

    Add files via upload

---
+++ backup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+l_date=$(date +%y%b%d)
+
+# get backup type
+echo "What would you like to update? (type 'types' to get the types)"
+read backupType
+echo ""
+
+# get location backups go
+source ./backupsLocation.cfg
+
+if [ "$backupType" = "types" ]
+then
+    echo "Full backup - 'full'"
+    echo "All media devices - 'media'"
+    echo "Home directory - 'home'"
+    echo "Custom directory - 'custom'"
+fi
+
+if [ "$backupType" = "full" ]
+then
+    tar -cvf "$location"/${l_date}_full-backup.tar /
+fi
+
+if [ "$backupType" = "media" ]
+then
+    tar -cvf "$location"/${l_date}_media-backup.tar /media/
+fi
+
+if [ "$backupType" = "home" ]
+then
+    tar -cvf "$location"/${l_date}_home-backup.tar /home/
+fi
+
+if [ "$backupType" = "custom" ]
+then
+    echo "What file would you like to back up?"
+    read backupDir
+    tar -cvf "$location"/${l_date}_custom-backup.tar "$backupDir"
+fi
+
+#tar -cvf /media/m/TOSHIBA EXT/linux_backups/backup_${l_date}.tar /
